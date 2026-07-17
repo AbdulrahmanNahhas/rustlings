02fd5c1f49ffe2e74bc8488828abfea766799e6f
AbdulrahmanNahhas <nahhas909@protonmail.com>
2026-07-17 16:08:44 +0300

    Update devenv.nix

--- devenv.nix
+++ devenv.nix
@@ -1,32 +1,17 @@
+{ ... }:
 {
-  # pkgs,
-  ...
-}:
-{
-  # https://devenv.sh/languages/
   languages.rust = {
     enable = true;
-    channel = "nixpkgs"; # default
+    channel = "nixpkgs";
   };
 
-  # https://devenv.sh/packages/
-  # packages = [ pkgs.git ];
-
-  # https://devenv.sh/basics/
   enterShell = ''
     rustc --version
     cargo --version
   '';
 
-  # https://devenv.sh/tests/
   enterTest = ''
     echo "Running tests"
     cargo --version | grep --color=auto "cargo"
   '';
-
-  # https://devenv.sh/git-hooks/
-  # git-hooks.hooks.rustfmt.enable = true;
-  # git-hooks.hooks.clippy.enable = true;
-
-  # See full reference at https://devenv.sh/reference/options/
 }
